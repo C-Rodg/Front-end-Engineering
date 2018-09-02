c0e49b852d873ebf5629fe2c478e90af0ed38b74
C-Rodg <C-Rodg@users.noreply.github.com>
2018-09-02 14:05:12 -0700

    Add general sort info

---
+++ sorts/_generalSortInfo.js
@@ -0,0 +1,9 @@
+// Sorts Speeds (fastest to slowest):
+// 1.)[*R] Merge Sort O(n * log n)
+// 2.)[*R] Quick Sort O(n * log n) - if already sorted will be O(n^2) though
+// 3.) Insertion Sort O(n^2) - works well with nearly sorted arrays
+// 4.) Selection Sort O(n^2)
+// 5.) Bubble Sort O(n^2)
+
+// ----------------------------
+// [*R] = recursive
