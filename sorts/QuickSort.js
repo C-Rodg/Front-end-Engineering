2a009bd1ae895952bb432aeff0937e341a929036
C-Rodg <C-Rodg@users.noreply.github.com>
2018-09-21 21:36:58 -0700

    Added in-place quicksort

--- sorts/QuickSort.js
+++ sorts/QuickSort.js
@@ -1,5 +1,5 @@
 // Runtime: O(n * log n)
-// can potentially get to O(n^2) if already sorted though : (
+// can potentially get to O(n^2) if already sorted though-that is why it's best to use random pivot
 
 // Summary - find pivot, create arrays for left and right of it
 // then call recursively on those while placing pivot in between them
@@ -50,4 +50,40 @@ function quickSortRandomPivot(arr) {
 		}
 	}
 	return [...quickSortRandomPivot(left), pivot, ...quickSortRandomPivot(right)];
+}
+
+// ---------- IN PLACE QUICK SORT -------- //
+
+// This function chooses a pivot and then
+// splits the array in place between left of pivot
+// and right of pivot.  Returns the pivot index.
+
+// Space complexity here: O(log n)
+
+function pivotHelper(arr, startIdx = 0, endIdx = arr.length - 1) {
+	let pivotValue = arr[startIdx];
+	let swapIdx = 0;
+	for (let i = startIdx + 1; i < arr.length; i++) {
+		if (arr[i] < pivotValue) {
+			swapIdx += 1;
+			//[arr[swapIdx], arr[i]] = [arr[i], arr[swapIdx]];
+			const lesser = arr[i];
+			arr[i] = arr[swapIdx];
+			arr[swapIdx] = lesser;
+		}
+	}
+
+	const lesser = arr[swapIdx];
+	arr[swapIdx] = arr[startIdx];
+	arr[startIdx] = lesser;
+	return swapIdx;
+}
+
+function quickSortInPlace(arr, left = 0, right = arr.length - 1) {
+	if (left < right) {
+		let pivotIdx = pivotHelper(arr, left, right);
+		quickSortInPlace(arr, left, pivotIdx - 1);
+		quickSortInPlace(arr, pivotIdx + 1, right);
+	}
+	return arr;
 }
