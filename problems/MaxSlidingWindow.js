9d57ddd0254237c89f1cd929d171b9a5446bc05f
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-01 21:55:01 -0700

    Added 2 new array questions

---
+++ problems/MaxSlidingWindow.js
@@ -0,0 +1,13 @@
+// Given a large array of integers and a window of size w,
+// find current maximum in the window as the window
+// slides through the entire array.
+
+// Input: [2, -1, 5, 0, 8], window size = 3
+// Output: [5, 5, 8]
+
+// Runtime: O(n)
+// Memory: O(w) where 'w' is the window width
+
+function findSlidingMaxWindow(arr, windowSize) {
+	// TODO:
+}
---
+++ problems/RotatedBinarySearch.js
@@ -0,0 +1,8 @@
+// Search a given number in a sorted
+// array that has been rotated by some arbitrary number.
+
+// Input: [6,7,1,2,3,4,5],3
+// Output: index 4
+
+// Runtime: O(log n)
+// Memory: O(log n) or O(1) if using iterative
