a1e7aa2fe03d66b04e1d813eefef1f560a144073
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-02 21:52:10 -0700

    Started restructuring questions and answers sections

---
+++ problems/questions/RotateArray.js
@@ -0,0 +1,9 @@
+// Given an array of integers, rotate the array by N elements.
+
+// Input: [1,2,3,4,5,6], 3
+// Output: [4,5,6,1,2,3]
+
+// Runtime: O(n)
+// Memory: O(1)
+
+// Bonus: use modulus to normalize rotations & determine only positive rotations
