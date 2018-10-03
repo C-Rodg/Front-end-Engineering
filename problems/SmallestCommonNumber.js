e01a8db091252898997e8d16083b18833c89de33
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-02 21:06:57 -0700

    Smallest common number problem added

---
+++ problems/SmallestCommonNumber.js
@@ -0,0 +1,8 @@
+// Given three integer arrays sorted in ascending order,
+// find the smallest number that is common in all three arrays.
+
+// Input: [-1, 4, 5], [0, 2, 4], [3, 4, 8]
+// Output: 4
+
+// Runtime: O(n)
+// Memory: O(1)
