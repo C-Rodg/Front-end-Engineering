e7b7b192fd31006876b0d8518081ffbc6bfdd7f3
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-18 19:24:42 -0700

    Added new BST problems

---
+++ problems/questions/DeleteZeroSumBST.js
@@ -0,0 +1,9 @@
+// Given root of a binary tree delete
+// any subtrees whose nodes sum up to zero.
+
+// Runtime: O(n)
+// Space: O(h) - because recursive solution will
+// take up the space of the height of the tree.  It
+// will be O(log n) for balanced and O(n) in worst case
+
+// HINT - DFS (postorder)
---
+++ problems/questions/SerializeDeserializeBST.js
@@ -0,0 +1,8 @@
+// Serialize binary tree to a stream and then deserialize it
+// back to an in-memory tree such that original and deserialized trees are identical.
+
+//Serialize: write the tree in a file.
+//Deserialize: read from a file and reconstruct the tree in memory.
+
+// Runtime: O(n)
+// Space: O(log n)
