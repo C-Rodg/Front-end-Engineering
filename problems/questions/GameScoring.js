4a593c8a1b8d55d167e533fb40d4aa2fd9b55250
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-20 18:46:56 -0700

    Added another DP problem

---
+++ problems/questions/GameScoring.js
@@ -0,0 +1,7 @@
+// Difficulty: Hard
+// Topic: Memoization, Dynamic Programming
+
+// Imagine a game where a player can score
+// 1, 2, or 4 runs.  Given a score 'n',
+// find the total number of ways that score
+// can be reached.
