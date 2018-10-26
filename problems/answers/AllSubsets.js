09d20054e1e742e3bab3886cc9c0eeb998799c86
C-Rodg <C-Rodg@users.noreply.github.com>
2018-10-25 19:32:57 -0700

    Completed all subsets problem

---
+++ problems/answers/AllSubsets.js
@@ -0,0 +1,35 @@
+// 1.) Get number of subsets -> 2^n
+// 2.) Loop from 0 -> number of total subsets using 'i'
+// 3.) Calculate the binary representation of 'i' (properly pad number)
+// 4.) For the '1' digits in the binary representation, add the index of the '1' from the set to the set to add
+// 5.) Return the set of sets
+
+function getAllSubsets(...args) {
+	let numberOfSubsets = Math.pow(2, args.length);
+	const result = [];
+	for (let i = 0; i < numberOfSubsets; i++) {
+		const newSet = [];
+		const binaryString = getBinaryString(i).padStart(args.length);
+		for (let c = 0; c < binaryString.length; c++) {
+			if (binaryString[c] === '1') {
+				newSet.push(args[c]);
+			}
+		}
+		result.push(newSet);
+	}
+}
+
+// Helper to get binary string
+function getBinaryString(num) {
+	return Number(num).toString(2);
+}
+
+// Alternative to get the binary string is to shift bits
+function getBit(num, bit) {
+	let temp = 1 << bit;
+	temp = temp & num;
+	if (temp === 0) {
+		return 0;
+	}
+	return 1;
+}
--- problems/questions/AllSubsets.js
+++ problems/questions/AllSubsets.js
@@ -1,4 +1,5 @@
 // Difficulty: HARD
+// Topic: Permutation
 
 // Given a set of integers
 // return all possible subsets of that set
@@ -8,4 +9,4 @@
 // INPUT [4,2,3]
 // OUTPUT: [], [2], [3], [2,3], [4], [2,4], [3,4], [2,3,4]
 
-// Subsets is a 2^n where n is set length formula
+// Subsets is a 2^n where n is the number of integers in a given set
