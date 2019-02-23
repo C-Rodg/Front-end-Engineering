cbdfd74ea0d035e523e5001be59ba5c763ea6aa4
C-Rodg <C-Rodg@users.noreply.github.com>
2019-02-23 15:05:46 -0800

    Added merge k sorted list question

---
+++ problems/answers/MergeKSortedLists.js
@@ -0,0 +1,6 @@
+// 1.) Create Min Heap structure with 'insert' and 'removeMin' methods
+// 2.) Loop through lists and add first pointer to the Min Heap
+// 3.) Create a new linked list with a dummy head pointer
+// 4.) While MinHeap is not empty, remove minimum item and add to new linked list.
+// 4b.) if removed item has a 'next' node, add it to MinHeap and repeat
+// 5.) return dummyHead.next
---
+++ problems/questions/MergeKSortedLists.js
@@ -0,0 +1,10 @@
+// Difficulty: Hard
+// Topic: Min Heaps, pointers
+
+// Merge k sorted link lists and return it as one sorted list..
+
+// Input:
+// [[1>4>5], [1>3>4], [2>6]]
+
+// Output:
+// [1>1>2>3>4>4>5>6]
