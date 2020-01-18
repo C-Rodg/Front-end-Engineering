0d1212f1e0755713952623e45f54b24b773843e2
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-17 20:40:53 -0800

    Queue restruction by height solved.

---
+++ problems/answers/Queues/QueueReconstructionByHeight.js
@@ -0,0 +1,23 @@
+function queueReconstruction(arr) {
+	// Sort it from max height to min height and secondarily from 0 people infront to most people infront
+	arr.sort((a, b) => {
+		if (a[0] > b[0]) {
+			return -1;
+		} else if (a[0] < b[0]) {
+			return 1;
+		} else if (a[1] > b[1]) {
+			return 1;
+		} else if (a[1] < b[1]) {
+			return -1;
+		}
+		return 0;
+	});
+
+	const q = [];
+	// Insert each person.  Since we know it's tallest > smallest, we know that inserting a new person will not mess up our order.
+	for (let person of arr) {
+		const position = person[1];
+		q.splice(position, 0, person);
+	}
+	return q;
+}
---
+++ problems/questions/Queues/QueueReconstructionByHeight.js
@@ -0,0 +1,9 @@
+// Suppose you have a random list of people standing in a queue.  Each person is described
+// by a pair of integers (h, k) where h is height and k is the number of people in front
+// of this person that have a height greater than or equal to h.  Properly sort the queue.
+
+// Input:
+// [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+// Output
+// [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
