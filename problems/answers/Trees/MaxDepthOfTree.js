b9becacc9840c734b6a72656c97b40f0904dd187
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-21 19:29:22 -0800

    Add iterative solution for max tree depth

--- problems/answers/Trees/MaxDepthOfTree.js
+++ problems/answers/Trees/MaxDepthOfTree.js
@@ -11,3 +11,28 @@ function maxDepthOfTree(node) {
 	// Return the maximum left or right height + 1 for this node
 	return Math.max(leftDepth, rightDepth) + 1;
 }
+
+function maxDepthIterative(root) {
+	const stack = [];
+
+	// Track the biggest depth
+	let depth = 0;
+
+	// Use a tuple that contains the node and the tree depth
+	stack.push([1, root]);
+
+	// While there is nodes to process
+	while (stack.length) {
+		const [currentDepth, currentNode] = stack.pop();
+		if (currentNode) {
+			// Determine if this depth is greater than the previous max depth
+			depth = Math.max(depth, currentDepth);
+
+			// Add the children to the stack
+			stack.push([currentDepth + 1, currentNode.left]);
+			stack.push([currentDepth + 1, currentNode.right]);
+		}
+	}
+
+	return depth;
+}
