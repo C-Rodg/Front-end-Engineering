51bfc205a3d7591c912d89fa76bfaeb484944595
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-22 17:15:09 -0800

    Add serialize and deserialize methods

---
+++ problems/answers/Trees/SerializeDeserialize.js
@@ -0,0 +1,50 @@
+function serialize(root) {
+	const result = [];
+	function serializeHelper(node) {
+		if (!node) {
+			result.push(null);
+			return;
+		}
+		// Visit the current node
+		result.push(node.val);
+
+		// Visit the left & right nodes
+		serializeHelper(node.left);
+		serializeHelper(node.right);
+	}
+
+	// run the helper
+	serializeHelper(root);
+
+	// clear the nulls at the end
+	while (result[result.length - 1] === null) {
+		result.pop();
+	}
+
+	// return the array as a string
+	return JSON.stringify(arr);
+}
+
+function deserialize(str) {
+	const arr = JSON.parse(str);
+	function deserializeHelper(values) {
+		// Take the values one by one
+		const val = values.shift();
+
+		// Empty values should just be set to null, not as a Node
+		if (!val) {
+			return null;
+		}
+
+		// create the node
+		const newNode = new Node(val);
+
+		// recurse both left and right
+		newNode.left = deserializeHelper(values);
+		newNode.right = deserializeHelper(values);
+
+		return newNode;
+	}
+
+	return deserializeHelper(arr);
+}
---
+++ problems/questions/Trees/SerializeDeserialize.js
@@ -0,0 +1,5 @@
+// Given a tree, find a way to serialize the tree into a string and then deserialize it back into a tree.
+
+// Hint 1: DFS
+
+// Hint 2: Preorder
