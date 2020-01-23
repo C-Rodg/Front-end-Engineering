d10f1b05bdcacb9af583defecf05021e9436fc95
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-22 17:18:41 -0800

    Removed old serialize methods

--- problems/questions/Trees/SerializeDeserialize.js
+++ problems/questions/Trees/SerializeDeserialize.js
@@ -3,3 +3,12 @@
 // Hint 1: DFS
 
 // Hint 2: Preorder
+
+// Solution #1: Recursive
+// Runtime: O(n)
+// Space: O(log n) -> based on tree height log n = balanced,
+//  n^ in worse case
+
+// Solution #2: Comparing Preorder to Inorder
+// Runtime: O(n log n) - O(n^2) in worst case
+// Memory: O(log n)
