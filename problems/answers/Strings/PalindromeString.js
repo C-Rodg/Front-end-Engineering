5982183a990e974cf446199b9c7884a0691d114d
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-24 18:35:15 -0800

    Complete check for palindrome

--- problems/answers/Strings/PalindromeString.js
+++ problems/answers/Strings/PalindromeString.js
@@ -1,3 +1,4 @@
+// Question 1:
 function palindromeEvery(str) {
 	return str.split('').every((char, i) => {
 		return char === str[str.length - i - 1];
@@ -11,6 +12,8 @@ function palindromeSplit(str) {
 		.join('');
 	return str === reversed;
 }
+
+// Question 2:
 
 // Palindromes can either start from one character or two.
 // Here we loop through each character and two characters and try to expand
@@ -38,4 +41,42 @@ function helperExpand(str, start, end) {
 	}
 
 	return str.substring(start + 1, end);
+}
+
+// Question 3:
+function canBePalindrome(str) {
+	const charMap = {};
+	// Create the character map
+	for (let char of str) {
+		if (!charMap[char]) {
+			charMap[char] = 0;
+		}
+		charMap += 1;
+	}
+
+	// Loop through the character map and build the first part of the result
+	let result = '';
+	let oddCharacter = '';
+	for (let char of Object.keys(charMap)) {
+		if (charMap[char] % 2 === 0) {
+			// add it to the result, but only half of them because the other half will be at the end
+			result += char.repeat(charMap[char] / 2);
+		} else if (!oddCharacter) {
+			// No odd character yet, we're allowed one
+			oddCharacter = char;
+		} else {
+			// Not possible, can't have two odd character counts
+			return false;
+		}
+	}
+
+	// Concat the entire palindrome with the first part, the odd character (if one exists), and the reversed first part
+	return (
+		result +
+		oddCharacter +
+		result
+			.split('')
+			.reverse()
+			.join('')
+	);
 }
--- problems/questions/Strings/PalindromeString.js
+++ problems/questions/Strings/PalindromeString.js
@@ -1,6 +1,7 @@
 // Difficulty: Easy
 // Topic: String Maps
 
+// Question 1:
 // Given a string, return T/F
 // for palindrome (same forward and backwards)
 
@@ -8,6 +9,7 @@ function isPalindrome(str) {
 	// TODO:
 }
 
+// Question 2:
 // Find the longest palindromic substring
 function longestPalindrome(str) {}
 
@@ -17,3 +19,13 @@ function longestPalindrome(str) {}
 
 // Input: "cbbd"
 // Output: "bb"
+
+// Question 3:
+// Given a string determine if there is a way to arrange the string
+// such that the string is a palindrome.  Otherwise return null.
+
+// Input: "mommo" -> true, we can switch the last 'm' and 'o'
+
+// Hint 1: Create a character map
+
+// Hint 2: a palindrome can either be with an even amount of charcters or an odd, but what happens if there are two odds?
