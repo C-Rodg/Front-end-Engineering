1864c1f8305b34cff661920aa4de92fd01cdcdeb
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-28 17:48:34 -0800

    Add floor ceiling BST

---
+++ problems/answers/Trees/FloorCeiling.js
@@ -0,0 +1,58 @@
+function findFloor(node, k) {
+	if (!node) {
+		return Number.MAX_SAFE_INTEGER;
+	}
+
+	// we have found the floor
+	if (node.val === k) {
+		return node.val;
+	}
+
+	// if node's value is greater than the key
+	if (node.val > k) {
+		return findFloor(node.left, k);
+	}
+
+	// otherwise the floor may be in the subtree or equal to the node's value
+	let floorValue = findFloor(node.right, k);
+	if (floorValue <= k) {
+		return floorValue;
+	} else {
+		return node.val;
+	}
+}
+
+function findCeil(node, k) {
+	if (!node) {
+		return null;
+	}
+
+	// If our key is greater than or equal to the current node, it must be in the right subtree
+	if (node.val <= k) {
+		return findCeil(node.right, k);
+	}
+
+	// Check the left subtree, if it isn't found, return the current nodes value
+	let ceilValue = findCeil(node.left, k);
+	if (!ceilValue) {
+		return node.val;
+	}
+	return ceilValue;
+}
+
+function findCeilIterative(root, k) {
+	const stack = [];
+	let curr = root;
+
+	while (curr) {
+		if (curr.val > k) {
+			stack.push(curr);
+			curr = curr.left;
+		}
+		if (curr.val <= k) {
+			curr = curr.right;
+		}
+	}
+
+	return stack.pop();
+}
