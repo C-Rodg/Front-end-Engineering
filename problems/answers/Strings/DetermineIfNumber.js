d4855d5ea894e1a6f8a6f1ad62ec0e3a8e456534
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-30 19:47:07 -0800

    Add determine if valid number state string parsing

---
+++ problems/answers/Strings/DetermineIfNumber.js
@@ -0,0 +1,53 @@
+// Declare your state's flow
+const STATES = {
+	BEGIN: ['NEGATIVE1', 'DIGIT1'],
+	NEGATIVE1: ['DIGIT1', 'DOT'],
+	DIGIT1: ['DIGIT1', 'DOT', 'E'],
+	DOT: ['DIGIT2'],
+	DIGIT2: ['DIGIT2', 'E'],
+	E: ['NEGATIVE2', 'DIGIT3'],
+	NEGATIVE2: ['DIGIT3'],
+	DIGIT3: ['DIGIT3']
+};
+
+// Lambda functions to determine when a state is allowed
+const LAMBDAS = {
+	NEGATIVE1: c => c === '-',
+	NEGATIVE2: c => c === '-',
+	DIGIT1: c => !isNaN(c),
+	DIGIT2: c => !isNaN(c),
+	DIGIT3: c => !isNaN(c),
+	E: c => c === 'e',
+	DOT: c => c === '.'
+};
+
+function isValidNumber(str) {
+	let currentState = 'BEGIN';
+	// Loop through the characters of the string
+	for (let char of str) {
+		let nextState = null;
+		// Loop through the possible next states
+		for (let potentialNextState of STATES[currentState]) {
+			// Check if it's a valid character to enter the state
+			if (LAMBDAS[potentialNextState](char)) {
+				// We have found a valid next state
+				nextState = potentialNextState;
+				break;
+			}
+		}
+
+		// If we didn't find a next state, it's invalid
+		if (nextState === null) {
+			return false;
+		}
+		// Set the next state to the current state
+		currentState = nextState;
+	}
+
+	// Check if we're in a valid end state
+	return (
+		currentState === 'DIGIT1' ||
+		currentState === 'DIGIT2' ||
+		currentState === 'DIGIT3'
+	);
+}
---
+++ problems/questions/Strings/DetermineIfNumber.js
@@ -0,0 +1,10 @@
+// Given a string, determine if the string is a number.
+// Parsing or built-ins are not allowed.
+
+// Valid:
+// 123
+// 1.23
+// -1.23
+// 1.5e7
+
+// Hint 1: State machine
