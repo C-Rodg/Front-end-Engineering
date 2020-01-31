7b2c6d536bf54d6a0a4396dd1401e566856054cf
C-Rodg <C-Rodg@users.noreply.github.com>
2020-01-30 20:25:33 -0800

    Add sorted squares problem and solution

---
+++ problems/answers/Stacks/SortedSquareNumbers.js
@@ -0,0 +1,29 @@
+function sortedSquareNumbers(nums) {
+	const stack = [];
+	const results = [];
+
+	for (let num of nums) {
+		// Create the square
+		const square = num * num;
+
+		// Check if the original number is negative or positive
+		if (num < 0) {
+			stack.push(square);
+		}
+
+		// Check if anything on the stack is less than the current square
+		while (stack.length && stack[stack.length - 1] <= square) {
+			results.push(stack.pop());
+		}
+
+		// Add the current squared item
+		results.push(square);
+	}
+
+	// Check if we have any remaining items in the stack
+	while (stack.length) {
+		results.push(stack.pop());
+	}
+
+	return results;
+}
---
+++ problems/questions/Stacks/SortedSquareNumbers.js
@@ -0,0 +1,7 @@
+// Given a list of sorted numbers, return a list of the sorted squares.
+// Beware of negatives!
+
+// Input: [-5, -3, -1, 0, 1, 4, 5]
+// Ouput: [0, 1, 1, 9, 16, 25, 25]
+
+// Hint 1: Use a stack for certain numbers...
