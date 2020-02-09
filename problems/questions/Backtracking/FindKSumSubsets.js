d4045eafea27c11f8072a371edffbc3b2b5538e4
C-Rodg <C-Rodg@users.noreply.github.com>
2020-02-08 21:33:11 -0800

    Wrote hints for find k sum subsets problem

---
+++ problems/questions/Backtracking/FindKSumSubsets.js
@@ -0,0 +1,6 @@
+// Given an array of positive integers, find all subsets of
+// that array that add up to a value 'k'.
+
+// Hint 1: What are the choices for each element at index 'i'?
+
+// Hint 2: What are the base cases?
