c53ff1e0d9f3128aeb57bc1e8a2ff14293c7abba
C-Rodg <C-Rodg@users.noreply.github.com>
2020-02-16 14:30:27 -0800

    Add stair climbing solution

--- problems/questions/DynamicProgramming/StairClimbing.js
+++ problems/questions/DynamicProgramming/StairClimbing.js
@@ -5,3 +5,12 @@
 // climb 1 or 2 stairs at a time
 
 // Bonus:  What if you can now climb 1, 2, or 3 stairs at a time?
+
+function numberOfWaysToClimb(n) {
+	return Number;
+}
+
+// Input: 4
+// Output: 5 because [1,1,1,1], [1,1,2], [1,2,1], and [2,2]
+
+// Hint 1: Number of ways to climb 1 stair? What about 2?
